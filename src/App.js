2ccc54c8f1bd0d7abe887bf0e79d85083412a01b
KeshavKumar Mishra <87552836+keshavmishra2@users.noreply.github.com>
2022-03-16 17:33:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import { Navbar } from './components/Navbar';
+import {Routes, Route} from "react-router-dom"
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { MoviesDashboard } from './components/MoviesDashboard';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path={"/"} element={<Home/>}></Route>
+        <Route path={"/about"} element={<About/>}></Route>
+        <Route path={"/movies"} eleent={<MoviesDashboard/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MoviesDashboard.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect,useState } from "react";
+import axios from "axios"
+import './MovieDashboard.css'
+
+
+
+//const genres = ["Comedy", "Thriller", "Drama", "Documentary", "Children"];
+
+export const MoviesDashboard = () => {
+  const [data,setdata] = useState([]);
+    const getData = ()=>{
+      axios.get(`https://movie-fake-server.herokuapp.com/data`)
+      .then(response=>{
+        setdata(response.data);
+    });
+    }
+
+  
+  useEffect(() => {
+   getData();
+
+  }, []);
+
+
+console.log(data.movie_name)
+//    filter by genre 
+const handleFilter = (e)=>{
+    // dispach filterby genre action to the store
+}
+
+  return (
+    <>
+      <h2>Movies</h2>
+      <select  onChange={handleFilter}>
+        {/* map through the filter  */}
+      </select>
+      <div className = "movies-list">
+         {data.map((e)=>(
+           <div>
+             <h1>{e.movie_name}</h1>
+             <h2>{e.genre}</h2>
+             <p>Rating-{e.rating}</p>
+             <p>Release date -{e.release_date}</p>
+             <img alt="" src={e.image_url}/>
+           </div>
+         ))}
+      </div>
+    </>
+  );
+};
